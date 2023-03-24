1468b158a612a7dddf320c0e2d75feed392038a9
acse-ra922 <110524155+acse-ra922@users.noreply.github.com>
2023-03-24 11:06:06 +0000

    Update Tests.h

--- tests/Tests.h
+++ tests/Tests.h
@@ -1,1 +1,31 @@
+// Group-Dijkstra
+// Xuefei Mi (xm421)
+// Yuyang Wang (yw22)
+// Chaofan Wu (cw522)
+// Yi Yang (yy3222)
+// Rubab Atwal (ra922)
 
+//tests
+#include <iostream>
+#include <string>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+class Tests{
+
+public:
+       void test_automatic_color_balance();
+       void test_brightness();
+       void test_hist_equal();
+       void test_median_blur();
+       void test_grayscale();
+       void test_box_blur();
+       void test_gaussian_blur();
+       void test_sobel();
+       void test_prewitt();
+       void test_fracture();
+       void test_confuciusornis();
+       
+};
